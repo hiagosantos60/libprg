67190cfc392150585e5e4b33bc9e996532631da7
hiago <hiago.s2006@aluno.ifsc.edu.br>
2025-11-18 11:00:17 -0300

    criar arvore binaria

--- libprg/src/libprg/arvore_binaria.c
+++ libprg/src/libprg/arvore_binaria.c
@@ -7,3 +7,10 @@ typedef struct no_arvore {
     struct no_arvore *direita;
 } no_arvore;
 
+no_arvore* criar_no_arvore(int valor) {
+    no_arvore *no = (no_arvore*)malloc(sizeof(no_arvore));
+    no->valor = valor;
+    no->esquerda = no->direita = NULL;
+    return no;
+}
+
